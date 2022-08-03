bd694eb2ee096928251bfffc378aa5f7a7fff2b1
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-04 00:11:46 +0700

    Menambah html pada file home

--- home.php
+++ home.php
@@ -4,8 +4,51 @@ session_start();
 if (!isset($_SESSION["username"])) {
     header("location:login_form.php");
 }
-
-echo "Ini form Home";
 ?>
-
-<a href="logout.php">Logout</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+    nav{
+        padding-left: 100px!important;
+        padding-right: 100px!important;
+        background: #6665ee;
+        font-family: 'Poppins', sans-serif;
+    }
+    nav a.navbar-brand{
+        color: #fff;
+        font-size: 30px!important;
+        font-weight: 500;
+    }
+    button a{
+        color: #6665ee;
+        font-weight: 500;
+    }
+    button a:hover{
+        text-decoration: none;
+    }
+    h1{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        font-size: 50px;
+        font-weight: 600;
+    }
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+    <a class="navbar-brand" href="#">Brand name</a>
+    <button type="button" class="btn btn-light"><a href="logout.php">Logout</a></button>
+    </nav>
+    <h1>Welcome <?php echo $_SESSION['username'] ?></h1>
+    <br>
+</body>
+</html>
